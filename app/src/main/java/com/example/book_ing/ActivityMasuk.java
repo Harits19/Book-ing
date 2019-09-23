56ff75ba85cc4d74afb536369dd8d3e2bc870c81
Harits19 <38558796+Harits19@users.noreply.github.com>
2019-09-23 14:15:42 +0700

    first commit

---
+++ app/src/main/java/com/example/book_ing/ActivityMasuk.java
@@ -0,0 +1,14 @@
+package com.example.book_ing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityMasuk extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_masuk);
+    }
+}

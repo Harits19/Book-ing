4b4fd321f1948652c1466abcfb7b913be77508d7
Harits19 <38558796+Harits19@users.noreply.github.com>
2019-10-02 11:22:30 +0700

    checkpoint

--- app/src/main/java/com/example/book_ing/Activity/ActivityDetailPesan.java
+++ app/src/main/java/com/example/book_ing/Activity/ActivityDetailPesan.java
@@ -5,20 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class FragmentBeranda extends AppCompatActivity {
+public class ActivityDetailPesan extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_beranda);
+        setContentView(R.layout.activity_detail_pesan);
 
-        TextView textViewNama = (TextView) findViewById(R.id.textViewNama);
-        Bundle extras = getIntent().getExtras();
-        if (extras != null) {
-            String displayName = extras.getString("displayName");
-            String displayEmail = extras.getString("displayEmail");
-            textViewNama.setText(displayName);
-            //The key argument here must match that used in the other activity
-        }
+
     }
 }

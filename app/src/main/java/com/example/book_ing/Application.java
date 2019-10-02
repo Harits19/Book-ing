9f4668be82f16626973d7c20abcfd83b429a009c
Harits19 <38558796+Harits19@users.noreply.github.com>
2019-10-02 11:09:03 +0700

    activity penukaran

--- app/src/main/java/com/example/book_ing/Application.java
+++ app/src/main/java/com/example/book_ing/Application.java
@@ -5,7 +5,7 @@ public final class Application extends android.app.Application {
     public void onCreate() {
         super.onCreate();
         FontsOverride.setDefaultFont(this, "DEFAULT", "fonts/product_sans_regular.ttf");
-        FontsOverride.setDefaultFont(this, "MONOSPACE", "fonts/arciform.otf");
+        FontsOverride.setDefaultFont(this, "MONOSPACE", "fonts/product_sans_regular.ttf");
         FontsOverride.setDefaultFont(this, "SERIF", "fonts/product_sans_regular.ttf");
         FontsOverride.setDefaultFont(this, "SANS_SERIF", "fonts/product_sans_regular.ttf");
     }
